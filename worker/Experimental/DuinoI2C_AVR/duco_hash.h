c5ad9d3534fbebb8723928e763132e73143b0e25
JK-Rolling <89238608+JK-Rolling@users.noreply.github.com>
2025-05-01 22:20:07 +0800

    beta

--- worker/Experimental/DuinoI2C_AVR/duco_hash.h
+++ worker/Experimental/DuinoI2C_AVR/duco_hash.h
@@ -38,83 +38,19 @@
  #define SHA1_H_
  
  #include <stdint.h>
- /** \def SHA1_HASH_BITS
-  * definees the size of a SHA-1 hash in bits 
-  */
- 
- /** \def SHA1_HASH_BYTES
-  * definees the size of a SHA-1 hash in bytes 
-  */
- 
- /** \def SHA1_BLOCK_BITS
-  * definees the size of a SHA-1 input block in bits 
-  */
- 
- /** \def SHA1_BLOCK_BYTES
-  * definees the size of a SHA-1 input block in bytes 
-  */
- #define SHA1_HASH_BITS  160
- #define SHA1_HASH_BYTES (SHA1_HASH_BITS/8)
- #define SHA1_BLOCK_BITS 512
- #define SHA1_BLOCK_BYTES (SHA1_BLOCK_BITS/8)
- 
- /** \typedef sha1_ctx_t
-  * \brief SHA-1 context type
-  * 
-  * A vatiable of this type may hold the state of a SHA-1 hashing process
-  */
- typedef struct {
-   uint32_t h[5];
-   uint64_t length;
- } sha1_ctx_t;
- 
- /** \typedef sha1_hash_t
-  * \brief hash value type
-  * A variable of this type may hold a SHA-1 hash value 
-  */
- /*
- typedef uint8_t sha1_hash_t[SHA1_HASH_BITS/8];
- */
  
  #ifdef __cplusplus
  extern "C" {
  #endif
- /** \fn sha1_init(sha1_ctx_t *state)
-  * \brief initializes a SHA-1 context
-  * This function sets a ::sha1_ctx_t variable to the initialization vector
-  * for SHA-1 hashing.
-  * \param state pointer to the SHA-1 context variable
+ /** \fn duco_hash_init(void *msg)
+  * \brief prehash for last_hash
+  * This function sets the initialization vector
+  * and ran first 10 loops of SHA-1 hashing.
+  * \param pointer to the last_hash
   */
- extern void sha1_init(sha1_ctx_t *state);
+extern void duco_hash_init(void *msg);
  
- /** \fn sha1_nextBlock(sha1_ctx_t *state, const void *block)
-  *  \brief process one input block
-  * This function processes one input block and updates the hash context 
-  * accordingly
-  * \param state pointer to the state variable to update
-  * \param block pointer to the message block to process
-  */
- extern void sha1_nextBlock (sha1_ctx_t *state, const void *block);
- 
- /** \fn sha1_lastBlock(sha1_ctx_t *state, const void *block, uint16_t length_b)
-  * \brief processes the given block and finalizes the context
-  * This function processes the last block in a SHA-1 hashing process.
-  * The block should have a maximum length of a single input block.
-  * \param state pointer to the state variable to update and finalize
-  * \param block pointer to themessage block to process
-  * \param length_b length of the message block in bits  
-  */
- extern void sha1_lastBlock (sha1_ctx_t *state, const void *block, uint16_t length_b);
- 
- /** \fn sha1_ctx2hash(sha1_hash_t *dest, sha1_ctx_t *state)
-  * \brief convert a state variable into an actual hash value
-  * Writes the hash value corresponding to the state to the memory pointed by dest.
-  * \param dest pointer to the hash value destination
-  * \param state pointer to the hash context
-  */ 
- extern void sha1_ctx2hash (void *dest, sha1_ctx_t *state);
- 
- /** \fn sha1(sha1_hash_t *dest, const void *msg, uint32_t length_b)
+ /** \fn duco_hash_try_nonce(void *buf, void *nonce)
   * \brief hashing a message which in located entirely in RAM
   * This function automatically hashes a message which is entirely in RAM with
   * the SHA-1 hashing algorithm.
@@ -122,14 +58,15 @@
   * \param msg  pointer to the message which should be hashed
   * \param length_b length of the message in bits
   */ 
- extern void sha1(void *dest, const void *msg, uint32_t length_b);
+extern void duco_hash_try_nonce(void *buf, void *nonce);
  
- extern uint32_t w10[10];
- extern uint8_t tempState[20];
+extern uint32_t w10[10];
+extern uint32_t tempState[5];
+extern uint32_t workState[5];
+extern char nonceStr[10+1];
+extern char buffer[90];
+#ifdef __cplusplus
+}
+#endif
  
- extern void duco_hash_init(void *msg);
- #ifdef __cplusplus
- }
- #endif
- 
- #endif /*SHA1_H_*/
+#endif /*SHA1_H_*/
